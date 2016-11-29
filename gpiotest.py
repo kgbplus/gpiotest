69284d1ee3498fba8226dbf594802fb8cd76f136
Roman Mindlin <mindlin@inbox.ru>
2016-11-29 18:50:41 +0300

    Lot of fixes

--- gpiotest.py
+++ gpiotest.py
@@ -1,25 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+#  gpiotest.py
+#
+#  Copyright 2016 Roman Mindlin <Roman@Mindlin.ru>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
 
 try:
     import RPi.GPIO as GPIO
 except RuntimeError:
     print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 
-import curses 
+import curses
 import random
 import time
 from datetime import datetime
 
 def termOn():
+    # Enable character buffering and echo
     curses.nocbreak()
     curses.echo()
 
 def termOff():
+    # Disable character buffering and echo
     curses.cbreak()
     curses.noecho()
 
 def MainScreen():
+    #Draw main screen
+    #coords - screen position of placeholders for pin states
     coords = [[6,15],[7,15],[8,15],[9,15],[10,15],[11,15],[12,15],[13,15],[14,15],
             [6,42],[7,42],[8,42],[9,42],[10,42],[11,42],[12,42],[13,42],[14,42],
             [6,69],[7,69],[8,69],[9,69],[10,69],[11,69],[12,69],[13,69]]
@@ -53,93 +78,139 @@ def MainScreen():
         myscreen.addstr(13,0, "|     GPIO9  =           |      GPIO18  =            |      GPIO27 =           |")
         myscreen.addstr(14,0, "|     GPIO10 =           |      GPIO19  =            |                         |")
     myscreen.addstr(15,0,  "--------------------------------------------------------------------------------")
-    myscreen.addstr(23,0,  "Q = Quit  P = Pause  D = Debounce  U = pullUp  1 = output_True 0 = output_False")
+    myscreen.addstr(21,0,  "--------------------------------------------------------------------------------")
+    myscreen.addstr(23,0,  "Q = Quit  P = Pause  D = Debounce  U = pullUp  O = Output I = Input")
 
+
+    #Print states and pullup status
     for i in range(gpio_num):
         state_text = "True" if gpio_state[i] else "False"
-        state_text = state_text + ("(1)" if gpio_pud[i] else "(0)")
-        myscreen.addstr(coords[i][0], coords[i][1], state_text, 
+        state_text = state_text + ("(^)" if gpio_pud[i] else "(v)")
+        myscreen.addstr(coords[i][0], coords[i][1], state_text,
                         curses.A_REVERSE if gpio_inout[i] else curses.A_NORMAL)
 
+    #Activity indicator
     myscreen.addstr(0,0, chr(int(random.random()*32) + 32))
 
+    # Print log strings
     logwindow.erase()
     for i in range(0,5):
-        logwindow.addstr(log[i])
+        logwindow.insstr(i,0,log[i])
 
-    myscreen.move(21,0)
+    #Set cursor position
+    myscreen.move(22,0)
     myscreen.refresh()
-    
+
 def SendToLog(LogMessage):
+    # Rotate log lines
     global log
-    
-    logwindow.erase()
-    for i in range(0,4):
+    for i in range(0,5):
         log[i] = log[i+1]
-        logwindow.addstr(log[i])
-    
     log[4] = LogMessage
-    logwindow.addstr(log[4])
+
+def PrintMsg(Msg):
+    # Print messages on bottom line of screen
+    msgwindow.erase()
+    msgwindow.addstr(Msg)
+    msgwindow.refresh()
 
 def CheckKeys():
+    # Keyboard events
     global debounce
     global on_pause
-    
+
     myscreen.nodelay(1)
     key = myscreen.getch()
     myscreen.nodelay(0)
-    
-    if key == ord('q') | ord('Q'):
+
+    if key == ord('q') or key == ord('Q'):
         raise KeyboardInterrupt
-    elif key == ord('p') | ord('P'):
-        myscreen.addstr(21,0,"Paused. Press P again to continue")
+    elif key == ord('p') or key == ord('P'):
+        PrintMsg("Paused. Press P again to continue")
         on_pause = 1
         while on_pause:
-            key = myscreen.getch()
-            if key == ord('p') | ord('P'):
+            key = msgwindow.getch()
+            if key == ord('p') or key == ord('P'):
                 on_pause = 0
-    elif key == ord('d') | ord('D'):
+    elif key == ord('d') or key == ord('D'):
         try:
             termOn()
-            myscreen.addstr(21,0,"Enter debounce value (ms): ")
-            debounce_ = debounce
-            debounce = int(myscreen.getstr())
-            if (debounce < 0 or debounce > 5000):
-                debounce = debounce_
+            PrintMsg("Enter debounce value (ms): ")
+            debounce_ = int(msgwindow.getstr())
+            if (debounce_ < 0 or debounce_ > 5000):
                 raise ValueError
             if (debounce_ != debounce):
-                initGpio()
+                debounce = debounce_
+                initGpio(0)
             termOff()
         except ValueError:
-            myscreen.addstr(22,0,"Value not in range")
+            PrintMsg("Value not in range")
             termOff()
-            myscreen.getch()
-    elif key == ord('u') | ord('U'):
+            msgwindow.getch()
+    elif key == ord('u') or key == ord('U'):
         try:
             termOn()
-            myscreen.addstr(21,0,"Enter GPIO line number: ")
-            num = int(myscreen.getstr())
-            if (num < 1 or num > gpio_num):
+            PrintMsg("Enter GPIO line number: ")
+            channel = int(msgwindow.getstr())
+            if not (channel in gpio_ch):
                 raise ValueError
-            if (gpio_inout[num-1]):
+            num = gpio_ch.index(channel)
+            if (gpio_inout[num]):
                 raise IOError
-            myscreen.addstr(21,0,"Enter 0 for GPIO.PUD_DOWN or 1 GPIO.PUD_UP: ")
-            pud = int(myscreen.getstr())
+            PrintMsg("Enter 0 for GPIO.PUD_DOWN or 1 GPIO.PUD_UP: ")
+            pud = int(msgwindow.getstr())
             if (pud != 1 and pud != 0):
                 raise ValueError
-            gpio_pud[num-1] = pud
-            initGpio()
+            gpio_pud[num] = pud
+            initGpio(0)
             termOff()
         except ValueError:
-            myscreen.addstr(22,0,"Value not in range")
+            PrintMsg("Value not in range")
             termOff()
-            myscreen.getch()
+            msgwindow.getch()
         except IOError:
-            myscreen.addstr(22,0,"Output line cannot be pulled up")
+            PrintMsg("Output line cannot be pulled up")
             termOff()
-            myscreen.getch()
+            msgwindow.getch()
+    elif key == ord('o') or key == ord('O'):
+        try:
+            termOn()
+            PrintMsg("Enter GPIO line number: ")
+            channel = int(msgwindow.getstr())
+            if not (channel in gpio_ch):
+                raise ValueError
+            num = gpio_ch.index(channel)
+            PrintMsg("Enter 0 for GPIO.LOW or 1 GPIO.HIGH: ")
+            val = int(msgwindow.getstr())
+            if (val != 1 and val != 0):
+                raise ValueError
+            gpio_inout[num] = 1
+            gpio_state[num] = val
+            GPIO.setup(channel, GPIO.OUT)
+            GPIO.output(channel,GPIO.HIGH if val else GPIO.LOW)
+            termOff()
+        except ValueError:
+            PrintMsg("Value not in range")
+            termOff()
+            msgwindow.getch()
+    elif key == ord('i') or key == ord('I'):
+        try:
+            termOn()
+            PrintMsg("Enter GPIO line number: ")
+            channel = int(msgwindow.getstr())
+            if not (channel in gpio_ch):
+                raise ValueError
+            num = gpio_ch.index(channel)
+            gpio_inout[num] = 0
+            initGpio(0)
+            termOff()
+        except ValueError:
+            PrintMsg("Value not in range")
+            termOff()
+            msgwindow.getch()
 
 def getPinFunctionName(pin):
+    # Unused
     functions = {GPIO.IN:'Input',
                  GPIO.OUT:'Output',
                  GPIO.I2C:'I2C',
@@ -147,10 +218,11 @@ def getPinFunctionName(pin):
                  GPIO.HARD_PWM:'HARD_PWM',
                  GPIO.SERIAL:'Serial',
                  GPIO.UNKNOWN:'Unknown'}
-                 
+
     return functions[GPIO.gpio_function(pin)]
-    
+
 def getRaspiModel(argument):
+    #Detect Raspberry Pi model
     switcher = {
         "0002": "Model B Revision 1.0 256Mb",
         "0003": "Model B Revision 1.0 + ECN0001 256Mb",
@@ -171,8 +243,9 @@ def getRaspiModel(argument):
         "a02082": "3 Model B 1Gb"
     }
     return switcher.get(argument, "not supported")
-    
+
 def getGpioNum(argument):
+    #Return number of GPIO lines
     switcher = {
         "0002": 17,
         "0003": 17,
@@ -194,30 +267,38 @@ def getGpioNum(argument):
     }
     return switcher.get(argument, 17)
 
-def initGpio():
-    curses.savetty()
+def initGpio(firstrun):
+    curses.savetty()  #Save screen
+
     #Init GPIO
-    GPIO.cleanup()
+    if not firstrun:
+        GPIO.cleanup()
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(0)
-    for i in range(gpio_num):
-        if (not gpio_inout[i]):
-            GPIO.setup(gpio_ch[i], GPIO.IN, pull_up_down=GPIO.PUD_DOWN if gpio_pud[i] == 0 else GPIO.PUD_UP)
-            GPIO.add_event_detect(gpio_ch[i], GPIO.BOTH, callback = gpio_callback, bouncetime = debounce) 
-            gpio_state[i] = GPIO.input(gpio_ch[i])
-    curses.resetty()
-    
+
+    #Init GPIO pins, set event_detect callbacks, save initial states etc.
+    for i,channel in enumerate(gpio_ch):
+        if not gpio_inout[i]:
+            GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN if gpio_pud[i] == 0 else GPIO.PUD_UP)
+            GPIO.add_event_detect(channel, GPIO.BOTH, callback = gpio_callback, bouncetime = debounce)
+            gpio_state[i] = GPIO.input(channel) # Primary state
+
+    curses.resetty()  # Restore screen
+
 def gpio_callback(channel):
+    # Callback fucntion - waiting for event, changing gpio states
     global gpio_state
-    if ((not gpio_inout[gpio_ch.index(channel)]) and (not on_pause)):
+
+    if not on_pause:
         gpio_state[gpio_ch.index(channel)] = GPIO.input(channel)
-	SendToLog(datetime.now().strftime("%Y-%b-%d %H:%M:%S")+": Channel " + str(channel) + 
+    SendToLog(datetime.now().strftime("%Y-%b-%d %H:%M:%S")+": Channel " + str(channel) +
           " changed " + ("(on)" if GPIO.input(channel) else "(off)")+"\n")
 
 try:
     #Init curses
     myscreen = curses.initscr()
-    logwindow = myscreen.subwin(6,80,17,0)
+    logwindow = myscreen.subwin(5,80,16,0)
+    msgwindow = myscreen.subwin(1,80,22,0)
     termOff()
 
     #Detect Raspberry Pi model
@@ -225,7 +306,8 @@ try:
     if (RaspiModel == "not supported"):
         raise NameError('hardware not supported')
 
-    #Tune GPIO parameters
+    #Detect GPIO parameters
+    #gpio_ch - array of GPIO lines numbers
     gpio_num = getGpioNum(GPIO.RPI_INFO['REVISION'])
     if (gpio_num == 17):
         gpio_ch = [0,1,4,7,8,9,10,11,14,15,17,18,21,22,23,24,25]
@@ -234,27 +316,32 @@ try:
     debounce = 200
 
     #Init vars
+    # 3 main structures:
+    # gpio_state stores current states of GPIO pins
+    # gpio_inout stores where pin configured to be IN or OUT
+    # gpio_pud stores pin's pullup state
     gpio_state = [0 for _ in range(gpio_num)]
     gpio_inout = [0 for _ in range(gpio_num)]
     gpio_pud = [0 for _ in range(gpio_num)]
     on_pause = 0
-    log = ['' for _ in range(5)]
+    log = ['' for _ in range(6)]
 
     #Init GPIO
-    initGpio()
+    initGpio(1)
 
     #Main loop
     while True:
         MainScreen()
         CheckKeys()
         time.sleep(0.1)
-    
+
 except KeyboardInterrupt:
+    # In case of keyboard interrupt
     myscreen.addstr(21,0,"Ctrl-C pressed")
     time.sleep(0.5)
     GPIO.cleanup()
-    
-finally: 
+
+finally:
+    # Reset terminal
     termOn()
     curses.endwin()
-    
